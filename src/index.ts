d794a7a36cb6b5126218e19dfb7fe243b3ea2c54
Karol Ścibior <karol.scibior98@gmail.com>
2019-11-18 19:57:28 +0100

    Add stuff idk

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
+import * as sessionAuth from './middleware/sessionAuth';
+import * as routes from './routes';
 
 dotenv.config();
 
-const app: express.Application = express();
 const port = process.env.SERVER_PORT;
+
+const app: express.Application = express();
 
 app.set('views', path.join( __dirname, 'views' ));
 app.set('view engine', 'ejs');
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.render('index');
-} );
+sessionAuth.register(app);
+
+routes.register(app);
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${ port }`);
